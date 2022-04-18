af8feddae1af6dc2615c61a1040b2e32ba7759ea
Diego Deberdt <git.diego.deberdt@gmail.com>
2022-04-18 21:47:12 +0200

    Fixed an issue with the rendering of an activity in portrait and landscape mode.

--- app/src/main/java/lu/uni/student/shoppinglist/activities/List/ListEditActivity.java
+++ app/src/main/java/lu/uni/student/shoppinglist/activities/List/ListEditActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -61,7 +62,13 @@ public class ListEditActivity extends AppCompatActivity {
         RecyclerView imageList = findViewById(R.id.imageList);
         imageList.setAdapter(listAdapter);
 
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 6);
+        GridLayoutManager layoutManager = null;
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            layoutManager = new GridLayoutManager(this, 5);
+        }
+        else{
+            layoutManager = new GridLayoutManager(this, 11);
+        }
         imageList.setLayoutManager(layoutManager);
     }
 
--- app/src/main/java/lu/uni/student/shoppinglist/activities/List/ListIcons.java
+++ app/src/main/java/lu/uni/student/shoppinglist/activities/List/ListIcons.java
@@ -4,18 +4,21 @@ import android.content.res.Resources;
 
 public class ListIcons {
     public static int[] getResourceIds(Resources resources, String packageName) {
-        int[] id = new int[31];
+        int[] id = new int[34];
 
         id[0] = resources.getIdentifier("aldi", "drawable", packageName);
         id[1] = resources.getIdentifier("auchan", "drawable", packageName);
-        id[2] = resources.getIdentifier("cactus", "drawable", packageName);
-        id[3] = resources.getIdentifier("carrefour", "drawable", packageName);
-        id[4] = resources.getIdentifier("cora", "drawable", packageName);
-        id[5] = resources.getIdentifier("lidl", "drawable", packageName);
+        id[2] = resources.getIdentifier("brico", "drawable", packageName);
+        id[3] = resources.getIdentifier("cactus", "drawable", packageName);
+        id[4] = resources.getIdentifier("carrefour", "drawable", packageName);
+        id[5] = resources.getIdentifier("cora", "drawable", packageName);
+        id[6] = resources.getIdentifier("decathlon", "drawable", packageName);
+        id[7] = resources.getIdentifier("hubo", "drawable", packageName);
+        id[8] = resources.getIdentifier("lidl", "drawable", packageName);
 
-        for(int i=6; i<31; i++) {
+        for(int i=0; i<25; i++) {
             int resourceId = resources.getIdentifier("type" + i, "drawable", packageName);
-            id[i] = resourceId;
+            id[9 + i] = resourceId;
         }
 
         return id;
